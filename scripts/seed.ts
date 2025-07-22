14d382def5db0b07e78027404068c242375d79c2
venoxdevpl <kamil@venoxdev.pl>
2025-07-22 16:33:17 +0200

    Update seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -10,19 +10,19 @@ async function bootstrap() {
     const app = await NestFactory.createApplicationContext(AppModule);
 
     const roles = app.get(RolesService);
-    await roles.seeder();
+    // await roles.seeder();
 
     const users = app.get(UsersService);
     await users.seeder();
 
     const allergens = app.get(AllergensService);
-    await allergens.seeder();
+    // await allergens.seeder();
 
     const categories = app.get(CategoriesService);
-    await categories.seeder();
+    // await categories.seeder();
 
     const companies = app.get(CompaniesService);
-    await companies.seeder();
+    // await companies.seeder();
 
     await app.close();
 }
